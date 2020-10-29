b748e0b58c252b54734ec245fb6fd153eb049249
Narendra52977 <68866931+Narendra52977@users.noreply.github.com>
2020-10-29 16:54:05 +0530

    r

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,10 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div>
-               //dont write anything here
-            </div>
-        )
-    }
+  render() {
+    return <div></div>;
+  }
 }
-
 
 export default App;
